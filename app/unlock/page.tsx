b53c75a7cce4cc7e45f5f3d2576d6257bfe2aeb7
izhharwidianto <izhhar.widianto@gmail.com>
2026-07-19 20:24:51 +0700

    Update page.tsx

---
+++ app/unlock/page.tsx
@@ -0,0 +1,7 @@
+export default function UnlockPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1>Unlock Page 🚀</h1>
+    </main>
+  );
+}
